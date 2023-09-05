f0e68e1b914d0c955a67559e6126855c0b01a9b7
zagreus10 <142361285+zagreus10@users.noreply.github.com>
2023-09-04 21:31:01 -0600

    agregamos descripcion del commit

--- pokemon2.py
+++ pokemon2.py
@@ -2,6 +2,11 @@ import requests
 #
 # Nombre : Johan Emmanuel Garay Garza
 # Matricula : 2001776
+# Script en python que consult el API de Pokemon
+# para listar los nombres de pokemon pero se le agrego
+# interaccion para que listaras mas pokemons segun se vaya requiriendo.
+# Contribuyo: Johan Emmanuel Garay Garza
+# Fecha: 04 de Septiembre de 2023
 
 def get_pokemons (url='https://pokeapi.co/api/v2/pokemon-form/', offset=0):
     args = {'offset':offset} if offset else {}
