f7ad11f575c25a8f50b1a941d6fc6fcb8a6c8aa9
Johan <zagreus04@gmail.com>
2023-09-04 21:26:03 -0600

    Se agrega script pokemon2.py al repositorio desde Windows

---
+++ pokemon2.py
@@ -0,0 +1,24 @@
+import requests
+#
+# Nombre : Johan Emmanuel Garay Garza
+# Matricula : 2001776
+
+def get_pokemons (url='https://pokeapi.co/api/v2/pokemon-form/', offset=0):
+    args = {'offset':offset} if offset else {}
+
+    response = requests.get(url, params=args)
+
+    if response.status_code == 200:
+        payload = response.json()
+        results = payload.get('results',[])
+
+        if results:
+            for pokemon in results:
+                name = pokemon['name']
+                print(name)
+        next = input ("¿Continuar listando? [Y/N]").lower()
+        if next == 'y':
+            get_pokemons(offset=offset+20)
+if __name__ == '__main__':
+    url = 'https://pokeapi.co/api/v2/pokemon-form/'
+    get_pokemons()
